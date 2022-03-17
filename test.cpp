2f8635d5dcec543b3b04185561db09fd61d3a8b2
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2022-03-17 11:59:30 +0100

    Add nvidia library and binary to test it.
    Extended test for basic consumption of a H264 stream and tracing client's
    callbacks.

--- test.cpp
+++ test.cpp
@@ -15,30 +15,116 @@
  * permissions and limitations under the License.
  */
 
+#include <atomic>
+#include <vector>
+#include <cerrno>
 #include <cstdlib>
+#include <cstdio>
+#include <cstring>
 #include <videoparser.h>
 #include <vk_video/vulkan_video_codecs_common.h>
+#include <vulkan/vulkan_beta.h>
+
+class Picture : public VkPicIf {
+public:
+    Picture() : m_refCount(0) {}
+
+    void AddRef() final {
+        assert(m_refCount >= 0);
+        ++m_refCount;
+    }
+
+    void Release() final {
+        assert(m_refCount > 0);
+        int32_t ref = --m_refCount;
+        if (ref == 0) {
+            decodeHeight = 0;
+            decodeWidth = 0;
+            decodeSuperResWidth = 0;
+        }
+    }
+
+    bool isAvailable() const { return m_refCount == 0; }
+
+private:
+    std::atomic<int32_t> m_refCount;
+};
 
 class VideoParserClient : public VkParserVideoDecodeClient {
 public:
-    int32_t BeginSequence(const VkParserSequenceInfo*) final { return 2; }
-    bool AllocPictureBuffer(VkPicIf**) final { return false; }
-    bool DecodePicture(VkParserPictureData*) final { return false; }
-    bool UpdatePictureParameters(VkPictureParameters*, VkSharedBaseObj<VkParserVideoRefCountBase>&, uint64_t) final { return false; }
-    bool DisplayPicture(VkPicIf*, int64_t) final { return false; }
-    void UnhandledNALU(const uint8_t*, int32_t) final { };
+    VideoParserClient() : m_dpb(32) { }
 
-    ~VideoParserClient() { }
+    int32_t BeginSequence(const VkParserSequenceInfo *info) final {
+        int32_t max = 16, conf = 1;
+
+        fprintf(stderr, "%s\n", __FUNCTION__);
+
+        if (info->eCodec == VK_VIDEO_CODEC_OPERATION_ENCODE_H264_BIT_EXT)
+            max++;
+
+        if (info->nMinNumDecodeSurfaces > 0)
+            conf += info->nMinNumDecodeSurfaces - (info->isSVC ? 3 : 1);
+        if (conf > max)
+            conf = max;
+
+        return std::min(conf, 17);
+    }
+
+    bool AllocPictureBuffer(VkPicIf **pic) final {
+        fprintf(stderr, "%s\n",__FUNCTION__);
+
+        for (auto &apic : m_dpb) {
+            if (apic.isAvailable()) {
+                apic.AddRef();
+                *pic = &apic;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool DecodePicture(VkParserPictureData *pic) final {
+        fprintf(stderr, "%s\n",__FUNCTION__);
+        return true;
+    }
+
+    bool UpdatePictureParameters(VkPictureParameters *params, VkSharedBaseObj<VkParserVideoRefCountBase>&, uint64_t) final {
+        fprintf(stderr, "%s\n",__FUNCTION__);
+        return true;
+    }
+
+    bool DisplayPicture(VkPicIf *pic, int64_t ts) final {
+        fprintf(stderr, "%s\n",__FUNCTION__);
+        return true;
+    }
+
+    void UnhandledNALU(const uint8_t*, int32_t) final {
+        fprintf(stderr, "%s\n",__FUNCTION__);
+    }
+
+    ~VideoParserClient() {
+        for (auto &pic : m_dpb)
+            assert (pic.isAvailable());
+    }
+
+private:
+    std::vector<Picture> m_dpb;
 };
 
-static bool parse()
+static bool parse(FILE *stream)
 {
     VulkanVideoDecodeParser* parser = nullptr;
+    VideoParserClient client = VideoParserClient();
     VkParserInitDecodeParameters params = {
          .interfaceVersion = VK_MAKE_VIDEO_STD_VERSION(0, 9, 1),
-         .pClient = new VideoParserClient(),
+         .pClient = &client,
     };
     bool ret;
+    unsigned char buf[BUFSIZ + 1];
+    size_t read;
+    int32_t parsed;
+    VkParserBitstreamPacket pkt;
 
     ret = CreateVulkanVideoDecodeParser(&parser, VK_VIDEO_CODEC_OPERATION_DECODE_H264_BIT_EXT, nullptr, 0);
     assert(ret);
@@ -50,6 +136,24 @@ static bool parse()
     if (!ret)
         return ret;
 
+    while (true) {
+        read = fread(buf, 1, BUFSIZ, stream);
+        if (read <= 0)
+            break;
+        pkt = (VkParserBitstreamPacket) {
+            .pByteStream = buf,
+            .nDataLength = static_cast<int32_t>(read),
+            .bEOS = read < BUFSIZ,
+        };
+
+        if (!parser->ParseByteStream(&pkt, &parsed)) {
+            fprintf(stderr, "failed to parse bitstream.\n");
+            break;
+        }
+
+        assert (pkt.nDataLength == parsed);
+    }
+
     ret = (parser->Release() == 0);
     assert(ret);
     return ret;
@@ -57,7 +161,24 @@ static bool parse()
 
 int main(int argc, char **argv)
 {
-    if (!parse())
+    FILE *file;
+
+    if (argc != 2) {
+        fprintf(stderr, "Could not ope file: %s.\n", argv[1]);
+        return EXIT_SUCCESS;
+    }
+
+    file = fopen(argv[1], "r");
+    if (!file) {
+        fprintf(stderr, "Unable to open: %s -- %s.\n", argv[1], strerror (errno));
         return EXIT_FAILURE;
+    }
+
+    if (!parse(file)) {
+        fclose(file);
+        return EXIT_FAILURE;
+    }
+
+    fclose(file);
     return EXIT_SUCCESS;
 }
